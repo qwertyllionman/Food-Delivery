bc7a8642ecce728c80bfc77c1126beae1d8505fe
Abdulaziz Axmadaliyev <qwertyllionman@gmail.com>
2025-09-23 12:24:33 +0500

    Logic docs is added to delivery

--- deliver/models.py
+++ deliver/models.py
@@ -7,7 +7,16 @@ class Deliver(Model):
     Attributes:
         user (fk) = deliver qaysi userga tegishli ekanligi
         restaurant (fk) = deliver qaysi restaurantga tegishli ekanligi
+        deliver (fk) = deliverdagi mahsulotlar
         status (enum(Preparing, Delivering, Delivered, Canceled)) = deliverning ayni damdagi statusi
         overall (decimal) = Buyurtmaning umumiy summasi
         destination (str) = Qayerga yetkazib berish
+    """
+
+class DeliverDetail(Model):
+    """
+    This is for many to many
+    Attributes:
+        deliver (fk) = Deliverga bog'lash
+        food (fk) = Foodga bog'lash
     """
--- deliver/views.py
+++ deliver/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+Delivery uchun CRUD operatsiyalari bo'ladi
+Create:
+    Delivery yaratilganda overall price avtomatik hisoblanadi restarantni delivery_fee siga qo'shiladi
+    Many to many bo'lib bog'lanadi Delivery Detail ga (qaysi food qaysi deliveryga tegishli ekanligi) 
+    
+GET:
+    barcha deliverylar olinadi (listda) (User olinganda unga tegishli deliverylarni ham chiqarish mumkin)
+    Ma'lum bir delivery ham id bo'yicha olinadi
+    
+PUT/PATCH:
+    Delivery statusi o'zgarib turadi (Preparing, Delivering, Delivered, Canceled)
+
+Ushbu joyda DELETE methodi bo'lishi mumkin edi lekin DELETE o'rniga historyga qo'shiladi COMPLETED
+yoki CANCELED statusli deliverylar 
+"""
